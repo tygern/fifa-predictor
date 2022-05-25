cb9fc972ba1977d4107a9c6daae6df3cd2aafe4d
Tyson Gern <tyson@initialcapacity.io>
2022-05-24 21:39:23 -0600

    Quiet debugger output during integration tests

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -58,4 +58,5 @@ Rails.application.configure do
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
+  DEBUGGER__.const_get(:CONFIG).set_config(log_level: "ERROR")
 end
